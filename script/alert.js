118247cff558cda2918ad7eedfe985dd146197bb
jackhasakeyboard <jackhasakeyboard@gmail.com>
2018-01-30 17:28:10 -0700

    Add taskbar favicons, updates depending on whether metal is adequite. No use for yellow warning yet

--- script/alert.js
+++ script/alert.js
@@ -8,6 +8,25 @@ $(document).ready(function() {
 
 			Object.keys(result).forEach(function(key, i) {
 				$('#metal li:nth-child(' + (i + 1) + ')').html(key + ': <span>' + result[key] + '</span>');
+
+				switch(key) {
+					case 'Reclaimed Metal':
+					case 'Scrap Metal':
+						if (result[key] < 3) {
+							chrome.browserAction.setIcon({
+								path: {
+									'16': '../img/warning_red.png'
+								}
+							});
+						} else {
+							chrome.browserAction.setIcon({
+								path: {
+									'16': '../img/ok.png'
+								}
+							});
+						}
+					default:
+				}
 			});
 		}
 	};
